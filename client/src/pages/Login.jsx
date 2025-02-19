74852e73cb1032dcba57b29eef4537496327d26e
HarshitEmpire <91474941+HarshitEmpire@users.noreply.github.com>
2025-02-19 16:35:07 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
     setIsLoading(true);
     try {
 
-      const res = await axios.post("https://w3villa-assignment-16no.onrender.com/api/auth/login", {
+      const res = await axios.post("https://task-manager-nu-murex.vercel.app/", {
         email,
         password,
       }, {withCredentials: true});
